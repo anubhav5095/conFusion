74490ee6ff937d39f3f81ddd789a487c001a8f74
anubhav5095 <anand_anubhav@yahoo.in>
2020-05-30 03:36:18 +0530

    npm scripts

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $('#mycarousel').carousel({interval:2000});
+    $('#carouselButton').click(function(){
+        if($('#carouselButton').children('span').hasClass('fa fa-pause'))
+        {
+            $('#mycarousel').carousel('pause');
+            $('#carouselButton').children('span').removeClass('fa-pause');
+            $('#carouselButton').children('span').addClass('fa-play');
+        }
+        else if($('#carouselButton').children('span').hasClass('fa fa-play'))
+        {
+            $('#mycarousel').carousel('cycle');
+            $('#carouselButton').children('span').removeClass('fa-play');
+            $('#carouselButton').children('span').addClass('fa-pause');
+        }
+
+        // for Modal
+        $('#reserveButton').click(function(){
+            $("#reserveTable").modal("show");
+        });
+        $('#loginButton').click(function(){
+            $("#loginModal").modal("show");
+        });
+    });
+});
+
